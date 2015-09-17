51cdfceed97c47b4490c4e0ae6ff70015d3bcbcb
Lauren Davis <londond9218@gmail.com>
2015-09-17 15:31:22 -0600

    agenda layout

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 angular.module("app", []); 
 
 var mainControllerFunc = function($scope) {
-	var today = Date();
-	$scope.appts = []; 
+	var today = new Date();
+	today.appts = []; 
 	$scope.days = [today];
 	$scope.userInput = false; 
 	$scope.selectDay = today; 
@@ -11,11 +11,14 @@ var mainControllerFunc = function($scope) {
 	$scope.theDays = function(num) {
 		var numOfDays = num; 
 		for (var i = 0; i < num; i++) {
-
-
+			var last = $scope.days[$scope.days.length - 1];
+			var next = new Date(last);
+			next.appts = [];
+			next.setDate(next.getDate() + 1);
+			$scope.days.push(next);
 		}
 	}
-
+	$scope.theDays(6);
 
 
 	$scope.addAppt= function(index) {
@@ -27,20 +30,26 @@ var mainControllerFunc = function($scope) {
 	}
 
 	$scope.submitAppt = function() { 
-	}
-	$scope.edit = function() {
+		$scope.selectDay.appts.push({
+			name        : $scope.newName,
+			description : $scope.newDescription,
+			editing     : false
+		});
+		
+		$scope.userInput = false;
+        $scope.newName = '';
+        $scope.newDescription = '';
 
+	}
+
+	$scope.edit = function(task) {
+		task.editing = !task.editing
+	}
+
+	$scope.change = function(event) {
+		event.preventDefault();
 	}
 }
-
-
-
-
-
-
-
-
-
 
 
 
